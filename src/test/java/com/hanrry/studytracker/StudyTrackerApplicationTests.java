dc801dc0467a1f0c4a7911b68ecc44fed66a0966
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-11 10:07:17 -0300

    feat: criacao das classes de teste

---
+++ src/test/java/com/hanrry/studytracker/StudyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanrry.studytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
