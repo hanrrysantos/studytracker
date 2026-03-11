18f83d7fe97a64c28ef4d351c1dba1ac121667f1
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-11 16:39:59 -0300

    feat: criacao do Mapper para tranformar entidade em DTO e tambem o contratario

---
+++ src/main/java/com/hanrry/studytracker/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.hanrry.studytracker.mapper;
+
+import com.hanrry.studytracker.dto.CreateUserRequestDTO;
+import com.hanrry.studytracker.dto.CreateUserResponseDTO;
+import com.hanrry.studytracker.entity.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    User toEntity(CreateUserRequestDTO createUserRequestDTO);
+
+    CreateUserResponseDTO toDTO(User user);
+
+    List<CreateUserResponseDTO> toDTOList(List<User> users);
+}
