504aad5d456c2ed960e7061b63e3bd3b67484d51
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-11 14:10:21 -0300

    feat: criacao da classe LessonProgress

---
+++ src/main/java/com/hanrry/studytracker/entity/LessonProgress.java
@@ -0,0 +1,97 @@
+package com.hanrry.studytracker.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "lesson_progress",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "lesson_id"})
+        }
+    )
+public class LessonProgress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private boolean completed;
+
+    @Column(name = "completed_at")
+    private LocalDateTime completedAt;
+
+    //Relacionamento com User
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    //Relacionamento com Lesson
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lesson_id", nullable = false)
+    private Lesson lesson;
+
+    public LessonProgress(){
+    }
+
+    public LessonProgress(boolean completed, LocalDateTime completedAt, User user, Lesson lesson) {
+        this.completed = completed;
+        this.completedAt = completedAt;
+        this.user = user;
+        this.lesson = lesson;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Lesson getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(Lesson lesson) {
+        this.lesson = lesson;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LessonProgress lessonProgress)) return false;
+        return id != null && id.equals(lessonProgress.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
