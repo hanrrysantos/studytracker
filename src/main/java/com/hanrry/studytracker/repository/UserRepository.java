05ffe18f1a11e3f0555314dc00365b0c9b70bf30
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-11 16:31:37 -0300

    feat: adicionando metodo para verificar se ja existe um email no banco de dados

--- src/main/java/com/hanrry/studytracker/repository/UserRepository.java
+++ src/main/java/com/hanrry/studytracker/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.hanrry.studytracker.repository;
 import com.hanrry.studytracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
 }
