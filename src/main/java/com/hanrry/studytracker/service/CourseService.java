08040edf15399a1b0689e8d82c145703d36df022
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-12 22:52:11 -0300

    criacao da classe CourseService com CRUD completo

---
+++ src/main/java/com/hanrry/studytracker/service/CourseService.java
@@ -0,0 +1,105 @@
+package com.hanrry.studytracker.service;
+
+import com.hanrry.studytracker.dto.CreateCourseRequestDTO;
+import com.hanrry.studytracker.dto.CreateCourseResponseDTO;
+import com.hanrry.studytracker.dto.UpdateCourseRequestDTO;
+import com.hanrry.studytracker.entity.Category;
+import com.hanrry.studytracker.entity.Course;
+import com.hanrry.studytracker.entity.User;
+import com.hanrry.studytracker.exception.ResourceNotFoundException;
+import com.hanrry.studytracker.mapper.CourseMapper;
+import com.hanrry.studytracker.repository.CategoryRepository;
+import com.hanrry.studytracker.repository.CourseRepository;
+import com.hanrry.studytracker.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static com.hanrry.studytracker.entity.CourseStatus.PLANNED;
+
+@Service
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+    private final CourseMapper courseMapper;
+
+    public CourseService(
+            CourseRepository courseRepository,UserRepository userRepository,
+            CategoryRepository categoryRepository, CourseMapper courseMapper
+    ){
+        this.courseRepository = courseRepository;
+        this.userRepository = userRepository;
+        this.categoryRepository = categoryRepository;
+        this.courseMapper = courseMapper;
+    }
+
+    public CreateCourseResponseDTO createCourse(CreateCourseRequestDTO request){
+        User user = userRepository.findById(request.userId()).orElseThrow(
+                () -> new ResourceNotFoundException("User not found with id: " + request.userId())
+        );
+
+        Category category = categoryRepository.findById(request.categoryId()).orElseThrow(
+                () -> new ResourceNotFoundException("Category not found with id: " + request.categoryId())
+        );
+
+        if(!category.getUser().getId().equals(user.getId())){
+            throw new IllegalArgumentException("Category does not belong to this user");
+        }
+
+        Course course = courseMapper.toEntity(request);
+        course.setCategory(category);
+        course.setUser(user);
+        course.setStatus(PLANNED);
+        course.setCreatedAt(LocalDateTime.now());
+        course.setUpdatedAt(LocalDateTime.now());
+
+        Course savedCourse = courseRepository.save(course);
+
+        return courseMapper.toDTO(savedCourse);
+    }
+
+    public CreateCourseResponseDTO findCourseById(Long id) {
+        Course course = courseRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Course not found with id: " + id)
+        );
+
+        return courseMapper.toDTO(course);
+    }
+
+    public Page<CreateCourseResponseDTO> findAllCourses(Pageable pageable){
+        Page<Course> coursePage = courseRepository.findAll(pageable);
+
+        return coursePage.map(courseMapper::toDTO);
+    }
+
+    public CreateCourseResponseDTO updateCourse(Long id, UpdateCourseRequestDTO request){
+        Course course = courseRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Course not found with id: " + id)
+        );
+
+        course.setTitle(request.title());
+        course.setDescription((request.description()));
+        course.setStatus(request.status());
+
+        if(request.categoryId() != null){
+            Category category = categoryRepository.findById(request.categoryId()).orElseThrow(
+                    () -> new ResourceNotFoundException("Category not found with id: " + request.categoryId())
+            );
+            course.setCategory(category);
+        }
+
+        course.setUpdatedAt(LocalDateTime.now());
+
+        Course savedCourse = courseRepository.save(course);
+
+        return courseMapper.toDTO(savedCourse);
+    }
+
+    public void deleteById(Long id){
+        courseRepository.deleteById(id);
+    }
+}
