e78dcf581e2129cbd5470889d3e607e7da09d5f6
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-16 08:48:10 -0300

    corrigindo injecao de dependencia

--- src/main/java/com/hanrry/studytracker/controller/AuthController.java
+++ src/main/java/com/hanrry/studytracker/controller/AuthController.java
@@ -18,13 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
-    public AuthController(AuthenticationManager authenticationManager){
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil){
         this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
     }
-
-    @Autowired
-    private JwtUtil jwtUtil;
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponseDTO> login(
