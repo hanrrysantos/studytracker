9c443faddf32bc708c537b2f37d9cf1753e81374
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-16 22:35:09 -0300

    corrigindo erros

--- src/main/resources/db/migration/V2__insert_admin_user.sql
+++ src/main/resources/db/migration/V2__insert_admin_user.sql
@@ -1,4 +1,5 @@
 INSERT INTO "users" (id, name, email, password, role, created_at) VALUES
 (1, 'Admin', 'admin@email.com', '$2a$10$IxJEWq4C7iBjL2EgqpTI9uzKvBRGTIJRboa0VLjXiQZIvPFRN/jfW', 'ADMIN', CURRENT_TIMESTAMP);
+ON CONFLICT (username) DO NOTHING;
 
 SELECT setval(pg_get_serial_sequence('users', 'id'), 1);
